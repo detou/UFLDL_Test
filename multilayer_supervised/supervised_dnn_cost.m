91e4cfbe6d08de9bc8c09d807f45b3328957d2d1
detou <chevalier.charles@gmail.com>
2016-11-09 19:20:58 +0100

    Working on dnn

--- multilayer_supervised/supervised_dnn_cost.m
+++ multilayer_supervised/supervised_dnn_cost.m
@@ -15,8 +15,26 @@ stack = params2stack(theta, ei);
 numHidden = numel(ei.layer_sizes) - 1;
 hAct = cell(numHidden+1, 1);
 gradStack = cell(numHidden+1, 1);
+
 %% forward prop
 %%% YOUR CODE HERE %%%
+
+for i = 1 : numHidden + 1
+    if i == 1
+        hAct{i} = bsxfun(@plus, stack{i}.W * data, stack{i}.b);
+    else
+        hAct{i} = bsxfun(@plus, stack{i}.W * hAct{i - 1}, stack{i}.b);
+    end
+    
+    % Apply activation function to hAct
+    switch ei.activation_fun
+        case 'logistic'
+            hAct{i} = sigmoid(hAct{i}); 
+    end
+end
+
+% Normalize probabilities
+pred_prob = bsxfun(@rdivide, exp(hAct{numHidden + 1}), sum(exp(hAct{numHidden + 1}), 1));
 
 %% return here if only predictions desired.
 if po
@@ -28,8 +46,18 @@ end;
 %% compute cost
 %%% YOUR CODE HERE %%%
 
+% Get a matrix with ones at i, k where y(i) == k
+I = sub2ind(size(pred_prob), labels', 1:size(pred_prob,2));
+O = zeros(size(pred_prob));
+O(I) = 1;
+
+% Calculate cost
+ceCost = 1 / (2 * size(data, 2)) * sum(sqrt(sum((O - pred_prob) .^ 2)));
+
 %% compute gradients using backpropagation
 %%% YOUR CODE HERE %%%
+
+
 
 %% compute weight penalty cost and gradient for non-bias terms
 %%% YOUR CODE HERE %%%
